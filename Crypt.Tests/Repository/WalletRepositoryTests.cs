cd2038d068e3389319324d87aa900d01ec516fbf
Anthony Silva <anthony.eldyn@gmail.com>
2024-10-28 10:54:09 -0300

    Fazendo um teste mais robusto para o GET

--- Crypt.Tests/Repository/WalletRepositoryTests.cs
+++ Crypt.Tests/Repository/WalletRepositoryTests.cs
@@ -1,5 +1,4 @@
 using AutoFixture;
-
 using Crypt.Domain;
 using Crypt.Repository.Repositories;
 using Crypt.Tests.Fixtures;
@@ -8,18 +7,48 @@ namespace Crypt.Tests.Repository
 {
     public class WalletRepositoryTests
     {
+        private readonly Fixture _fixture = BaseTests.CreateFixture();
+
         [Fact]
         public async void GetSingleShouldReturnAEntityInsertedInTheDatabase()
         {
             using (var ctx = new InMemoryDatabaseContext().Create())
             {
                 var repository = new WalletRepository(ctx);
-                var fixture = new Fixture().Create<Wallet>();
+                var fixture = _fixture.Create<Wallet>();
                 var result = await ctx.AddAsync(fixture);
                 await ctx.SaveChangesAsync();
 
                 var entity = await repository.GetSingle(e => e.Id == result.Entity.Id);
-                Console.WriteLine(entity.CreditCardNumber);
+                entity.Should().NotBeNull();
+                entity.UserDocument.Should().Be(fixture.UserDocument);
+            }
+        }
+
+        [Fact]
+        public async void WhenNoEntityIsFoundShouldThrowAnError()
+        {
+            using (var ctx = new InMemoryDatabaseContext().Create())
+            {
+                var repository = new WalletRepository(ctx);
+                var fixture = _fixture.Create<Wallet>();
+
+                Func<Task> action = async () => await repository.GetSingle(e => e.Id == fixture.Id);
+
+                await action.Should().ThrowAsync<Exception>("Entity not found!");
+            }
+        }
+
+        [Fact]
+        public async void GetSingleShouldThrowAnErrorWhenNoPredicateIsProvided()
+        {
+            using (var ctx = new InMemoryDatabaseContext().Create())
+            {
+                var repository = new WalletRepository(ctx);
+                Func<Task> action = async () => await repository.GetSingle(predicate: null!);
+                await action
+                    .Should()
+                    .ThrowAsync<ArgumentException>("Closure function not provided");
             }
         }
     }
